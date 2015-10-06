00ea1f167c2d71dd7f96595c009377a06477ca42
tigredonorte <tigredonorte3@gmail.com>
2015-10-06 18:39:59 -0300

    loadConfigClass

--- install/classes/inclasses/registerConfigurations.php
+++ install/classes/inclasses/registerConfigurations.php
@@ -34,52 +34,59 @@ class registerConfigurations extends classes\Classes\Object implements \install_
 
             private function LoadConfigClass($plugin){
                 $class  = "{$plugin}Configurations";
-                $file   = classes\Classes\Registered::getPluginLocation($plugin, true). "/Config/$class.php";
-                getTrueDir($file);
-                if(false === file_exists($file)){return $this->setErrorMessage("O arquivo da classe $class não foi encontrado no diretório de configurações do plugin $plugin!");}
-                require_once $file;
-                if(false === class_exists($class, false)){return $this->setErrorMessage("A classe $class não foi encontrada no plugin $plugin");}
+                if(false === $this->loadClass($class, $plugin)){return false;}
                 
                 //carrega o arquivo
                 $this->obj = new $class();
                 if(!$this->obj instanceof \classes\Classes\Options) {
-                    $this->setErrorMessage("A classe $class não é uma instância da classe Options.");
-                    return false;
+                    return $this->setErrorMessage("A classe $class não é uma instância da classe Options.");
                 }
                 return true;
             }
+            
+                    private function loadClass($class, $plugin){
+                        if(false !== class_exists($class, false)){return true;}
+                        $file   = classes\Classes\Registered::getPluginLocation($plugin, true). "/Config/$class.php";
+                        getTrueDir($file);
+                        if(false === file_exists($file)){return $this->setErrorMessage("O arquivo da classe $class não foi encontrado no diretório de configurações do plugin $plugin!");}
+                        require_once $file;
+                        if(false === class_exists($class, false)){
+                            return $this->setErrorMessage("A classe $class não foi encontrada no plugin $plugin");
+                        }
+                        return true;
+                    }
     
-    private function registerOne($plugin, $cod_plugin){
-        $this->cod_plugin = $cod_plugin;
-        $this->files      = $this->obj->getFiles();
-        $this->menu_itens = $this->obj->getMenu();
-        
-        $this->cfile->initPlugin($plugin, $cod_plugin);
-        foreach($this->files as $path => $arr){
-            if(!$this->cfile->insertConffile($arr, $path)){
-                $this->setAlertMessage($this->cfile->getMessages());
-                return false;
+            private function registerOne($plugin, $cod_plugin){
+                $this->cod_plugin = $cod_plugin;
+                $this->files      = $this->obj->getFiles();
+                $this->menu_itens = $this->obj->getMenu();
+
+                $this->cfile->initPlugin($plugin, $cod_plugin);
+                foreach($this->files as $path => $arr){
+                    if(!$this->cfile->insertConffile($arr, $path)){
+                        $this->setAlertMessage($this->cfile->getMessages());
+                        return false;
+                    }
+                }
+                $this->cfile->reset();
+                return true;
             }
-        }
-        $this->cfile->reset();
-        return true;
-    }
-    
-    private function registerMenu($plugin){
-        if(empty($this->menu_itens)) {return true;}
-        $insert_array = array();
-        foreach($this->menu_itens as &$post){
-            if(!isset($post['menuid'])){continue;}
-            $toinsert['menuid'] = $post['menuid'];
-            $toinsert['menu']   = isset($post['menu'])  ?$post['menu']  :ucfirst($post['menuid']);
-            $toinsert['url']    = isset($post['url'])   ?$post['url']   :'';
-            $toinsert['ordem']  = isset($post['ordem']) ?$post['ordem'] :100;
-            $toinsert['pai']    = isset($post['pai'])   ?$post['pai']   :"";
-            $toinsert['icon']   = isset($post['icon'])  ?$post['icon']  :"";
-            $toinsert['plugin'] = trim($plugin);
-            $insert_array[]     = $toinsert;
-        }
-        if(empty($insert_array)){return true;}
-        return $this->propagateMessage($this->menu, 'importDataFromArray', $insert_array, true);
-    }        
+
+            private function registerMenu($plugin){
+                if(empty($this->menu_itens)) {return true;}
+                $insert_array = array();
+                foreach($this->menu_itens as &$post){
+                    if(!isset($post['menuid'])){continue;}
+                    $toinsert['menuid'] = $post['menuid'];
+                    $toinsert['menu']   = isset($post['menu'])  ?$post['menu']  :ucfirst($post['menuid']);
+                    $toinsert['url']    = isset($post['url'])   ?$post['url']   :'';
+                    $toinsert['ordem']  = isset($post['ordem']) ?$post['ordem'] :100;
+                    $toinsert['pai']    = isset($post['pai'])   ?$post['pai']   :"";
+                    $toinsert['icon']   = isset($post['icon'])  ?$post['icon']  :"";
+                    $toinsert['plugin'] = trim($plugin);
+                    $insert_array[]     = $toinsert;
+                }
+                if(empty($insert_array)){return true;}
+                return $this->propagateMessage($this->menu, 'importDataFromArray', $insert_array, true);
+            }        
 }
